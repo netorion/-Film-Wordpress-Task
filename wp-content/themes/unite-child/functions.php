3a84aa81405aafa59aa68cbc2d8fe1e7823c3260
Victor Sylva <victorsylva@gmail.com>
2018-08-27 10:42:10 +0100

    called film custom post type from functions

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -3,4 +3,5 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+require_once( get_stylesheet_directory() . '/lib/film-custom-post.php' );
 ?>
